b7ad4d732e84ecc1315ce624799d868e9ddd1af3
Eelli002 <elijah_elliott@hotmail.com>
2021-04-22 14:02:40 -0700

    feature/banner

--- src/componets/Products/index.jsx
+++ src/componets/Products/index.jsx
@@ -1,10 +1,12 @@
 import Product from '../Product';
 import { Grid, Container } from '@material-ui/core';
+import Banner from '../Banner';
 
 const Products = ({ products }) => {
 
     return (
         <div>
+            <Banner />
             <Container id='products'>
                 <Grid container spacing={4}>
                     {products.map((product) => {
