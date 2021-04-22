b1351535726f8796baedea4e54126913c3edca12
Eelli002 <elijah_elliott@hotmail.com>
2021-04-21 19:57:53 -0700

    product cards

---
+++ src/componets/Products/index.jsx
@@ -0,0 +1,14 @@
+import Product from '../Product';
+
+const Products = ({ products }) => {
+
+    return (
+        <div>
+            {products.map((product) => {
+                return <Product product={product}/>;
+            })}
+        </div>
+    );
+};
+
+export default Products;
